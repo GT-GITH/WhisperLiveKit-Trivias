3de09d2dc304ffd4712deea227268f40d67c4b4f
GT-GITH <topcug1975@gmail.com>
2025-10-31 19:23:42 +0100

    X

--- whisperlivekit/basic_server.py
+++ whisperlivekit/basic_server.py
@@ -98,6 +98,9 @@ def main():
     """Entry point for the CLI command."""
     import uvicorn
     
+    uvicorn_kwargs["host"] = "0.0.0.0"
+    uvicorn_kwargs["port"] = 8000
+    
     uvicorn_kwargs = {
         "app": "whisperlivekit.basic_server:app",
         "host":args.host, 
@@ -121,8 +124,6 @@ def main():
     if args.forwarded_allow_ips:
         uvicorn_kwargs = { **uvicorn_kwargs, "forwarded_allow_ips" : args.forwarded_allow_ips }
 
-        uvicorn_kwargs["host"] = "0.0.0.0"
-        uvicorn_kwargs["port"] = 8000
 
     uvicorn.run(**uvicorn_kwargs)
  
