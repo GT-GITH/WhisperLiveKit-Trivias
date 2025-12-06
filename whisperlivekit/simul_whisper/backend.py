364c1b037e4eba40e8de01441785c1a52f54667d
GT-GITH <topcug1975@gmail.com>
2025-12-06 18:49:59 +0100

    silence reset prototype met nog meer extra debuglog

--- whisperlivekit/simul_whisper/backend.py
+++ whisperlivekit/simul_whisper/backend.py
@@ -42,6 +42,7 @@ class SimulStreamingOnlineProcessor:
     SAMPLING_RATE = 16000
 
     def __init__(self, asr, logfile=sys.stderr):
+        logger.error("🔥 SimulStreamingOnlineProcessor.__init__ CALLED 🔥")
         self.asr = asr
         self.logfile = logfile
         self.end = 0.0
